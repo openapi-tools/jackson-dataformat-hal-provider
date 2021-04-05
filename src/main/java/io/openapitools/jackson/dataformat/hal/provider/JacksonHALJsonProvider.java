6baa84d2fe0cc206c44bfd58b5fa3633e0ae7681
Thor Anker Kvisgård Lange <tal@nykredit.dk>
2021-04-05 16:18:46 +0200

    :memo: Adjusting javadoc a little bit

--- src/main/java/io/openapitools/jackson/dataformat/hal/provider/JacksonHALJsonProvider.java
+++ src/main/java/io/openapitools/jackson/dataformat/hal/provider/JacksonHALJsonProvider.java
@@ -16,12 +16,21 @@ import javax.ws.rs.ext.Provider;
 @Produces(JacksonHALJsonProvider.MEDIA_TYPE_APPLICATION_HAL_JSON)
 @Consumes(JacksonHALJsonProvider.MEDIA_TYPE_APPLICATION_HAL_JSON)
 public class JacksonHALJsonProvider extends JacksonJsonProvider {
+    /** Defining content type for JSON HAL */
     public static final String MEDIA_TYPE_APPLICATION_HAL_JSON = "application/hal+json";
 
+    /**
+     * Creating instance with default annotations which uses the {@link HALMapper}.
+     */
     public JacksonHALJsonProvider() {
         super(new HALMapper());
     }
 
+    /**
+     * Creating instance with custom list of annotations which uses the {@link HALMapper}.
+     * 
+     * @param annotationsToUse Sets of annotations (Jackson, JAXB) that provider should support
+     */
     public JacksonHALJsonProvider(Annotations[] annotationsToUse) {
         super(new HALMapper(), annotationsToUse);
     }
